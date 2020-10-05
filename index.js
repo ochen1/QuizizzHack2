6c1800aa4e7eb9c42b24c0eec5995e5d9fb2e112
Oliver Chen <o.chen1@share.epsb.ca>
2020-10-05 14:38:50 -0600

    🤦‍

--- index.js
+++ index.js
@@ -17,9 +17,9 @@
 
     // Inject required libraries
     let script;
-    for (let lib of ["https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"]) {
+    for (let lib of ["https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"]) {
         script = document.createElement("script");
-        script.src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js";
+        script.src = lib;
         script.type = "text/javascript";
         document.getElementsByTagName("head")[0].appendChild(script);
     }
