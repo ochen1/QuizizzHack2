048aa3135c5aa1bd710ef847279b8b46adf2dd1b
Oliver Chen <o.chen1@share.epsb.ca>
2020-10-12 11:41:23 -0600

    Add some unused error message modals

--- quizizz-utils/quizizz-modals.js
+++ quizizz-utils/quizizz-modals.js
@@ -18,4 +18,66 @@ class QuizizzModals {
 `
         );
     }
+
+    static createFatalErrorModal(errorcode) {
+        document.querySelector(".root-component").insertAdjacentHTML(
+            "beforeend",
+            `
+<div data-v-b36beb7c="" class="overlay-wrapper">
+    <div data-v-b36beb7c="" class="overlay overlay-rejoin-error">
+        <div data-v-b36beb7c="" class="image-container">
+            <img data-v-b36beb7c="" src="https://cf.quizizz.com/game/img/ui/invalid_game.png" alt="avocado">
+        </div>
+        <div data-v-b36beb7c="" class="text-container">
+            <span data-v-b36beb7c="">
+            An error has occurred. Please try again.
+            </span>
+            <span data-v-b36beb7c="" class="error-code"><br data-v-b36beb7c=""><br data-v-b36beb7c="">
+            If the issue persists, please share a screenshot on Github
+            <br data-v-b36beb7c="">
+            Error code - ${errorcode}.
+            </span>
+        </div>
+        <div data-v-b36beb7c="" class="btn-wrapper">
+            <a style="color: inherit; text-decoration: inherit;" href="https://github.com">Report an issue</a>
+        </div>
+    </div>
+</div>
+</div>
+`
+        );
+    }
+
+    static createFailureToReportErrorModal(errdata) {
+        let vdiv = document.createElement("div");
+        vdiv.innerText = vdiv.textContent = errdata; // HTML-escaped data
+        document.querySelector(".root-component").insertAdjacentHTML(
+            "beforeend",
+            `
+<div data-v-b36beb7c="" class="overlay-wrapper">
+    <div data-v-b36beb7c="" class="overlay overlay-rejoin-error">
+        <div data-v-b36beb7c="" class="image-container">
+            <img data-v-b36beb7c="" src="https://cf.quizizz.com/game/img/ui/disconnect.png" alt="disconnect">
+        </div>
+        <div data-v-b36beb7c="" class="text-container">
+            <span data-v-b36beb7c="">
+            Failed to automatically report the error!
+            </span>
+            <span data-v-b36beb7c="" class="error-code"><br data-v-b36beb7c=""><br data-v-b36beb7c="">
+            If the issue persists please share a screenshot on Github
+            <br data-v-b36beb7c="">
+            Please share the data below with us.
+            <br data-v-b36beb7c="">
+            <textarea rows="4" cols="50">${vdiv.innerHTML}</textarea>
+            </span>
+        </div>
+        <div data-v-b36beb7c="" class="btn-wrapper">
+            <a style="color: inherit; text-decoration: inherit;" href="https://github.com">Report an issue</a>
+        </div>
+    </div>
+</div>
+</div>
+`
+        );
+    }
 }
